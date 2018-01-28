8cd53e4b9187869b9ecdf8c1851e9a987d792829
andreas@useaquestion.co.uk <andreas@useaquestion.co.uk>
2018-01-28 08:28:59 +0000

    Better handling of 405 errors.

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,13 @@
+@extends('template')
+
+@section('content')
+<h2>405 - Oops! This method is not allowed!</h2>
+<div class="row">
+	<div class="col-12 col-md-4">
+		You are receiving this page because your web browser sent an unallowed method. If you don't know what this means, it's almost certainly a temporary error or a problem with the site. Try to do what you were doing again and if you see this error, please contact us to let us know. 
+	</div>
+	<div class="col-12 col-md-8">
+		<img src="/images/template/405.jpg" alt="405 image of a Peugeot. I'm hilarious" class="img-fluid" />
+	</div>
+</div>
+@endsection
