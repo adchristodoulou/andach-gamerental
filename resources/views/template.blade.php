f021124a19f57a6244d096a3727898ff14a199dc
Andreas Christodoulou <andreas@useaquestion.co.uk>
2018-04-03 19:32:57 +0100

    Add a cookie warning.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -53,7 +53,23 @@
     <meta property="twitter:url" content="{{ url()->current() }}"/>
     <meta property="twitter:image" content="@yield('image', '')" />
 
-
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+    <script>
+    window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#000"
+        },
+        "button": {
+          "background": "#f1d600"
+        }
+      },
+      "theme": "classic",
+      "position": "bottom-right"
+    })});
+    </script>
 
   </head>
 
